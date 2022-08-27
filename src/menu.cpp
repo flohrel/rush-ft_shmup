80b6af3bfcfa1ab75e5bff85a62a0578ae5ff2ff
Thibault NOULENS <tnoulens@e1r3p15.clusters.42paris.fr>
2022-08-27 14:09:32 +0200

    draft menu

---
+++ src/menu.cpp
@@ -0,0 +1,25 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   menu.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tnoulens <tnoulens@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/27 13:58:39 by tnoulens          #+#    #+#             */
+/*   Updated: 2022/08/27 14:07:58 by tnoulens         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <ncurses.h>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+
+void	welcome_menu(WINDOW *main)
+{
+	int		xmax;
+	int		ymax;
+	WINDOW	*wmenu;
+	
+	getmaxyx(stdscr, ymax, xmax);	
+}
