401c96a8cd5b73f1b69f13ac84b3797546460064
Florian OHREL <flohrel@student.42.fr>
2022-08-27 17:24:56 +0200

    level border collision ok

--- src/State.cpp
+++ src/State.cpp
@@ -12,20 +12,20 @@ void	State::update(Window window)
 	switch (cur_key)
 	{
 		case 'w':
-			if ( --player.y < 0 )
+			if ( --player.y == 0 )
 				player.y++;
 			break ;
 		case 's':
-			if ( ++player.y > window.height )
+			if ( ++player.y > window.height - 2 )
 				player.y--;
 			break ;
 		case 'a':
-			if ( (player.x -= 2) < 0 )
-				player.x += 2;
+			if ( (player.x -= 2) <= 0 )
+				player.x = 1;
 			break ;
 		case 'd':
-			if ( (player.x += 2) > window.width )
-				player.x -= 2;
+			if ( (player.x += 2) >= (window.width - 2) )
+				player.x = window.width - 3;
 			break ;
 		default:
 			break ;
