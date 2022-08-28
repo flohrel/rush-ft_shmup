6b3f93ecc8b2b0976f2f9c4d22dc82ff4c740135
Florian OHREL <flohrel@student.42.fr>
2022-08-28 23:40:56 +0200

    full vers

--- src/main.cpp
+++ src/main.cpp
@@ -85,9 +85,6 @@ int main()
 		game_state.score = 0;
 		game_state.life = 1;
 
-		// TEST enemy
-		//Enemy e(return_random(1, WIDTH - 2), 1);
-		//game_state.enemies.push_back(e);
 		// TIME
 		clock_t t;
 		double time_taken = 0.0;
@@ -98,19 +95,14 @@ int main()
 				break ;
 			if (return_random(0, 10000) > 9999)
 			{
-<<<<<<< HEAD
-				Enemy e(return_random(4, WIDTH - 4), return_random(1, HEIGHT - 20));
-=======
 				Enemy e(return_random(4, WIDTH - 8), return_random(4, HEIGHT - 20));
 				Object o(e.x, e.y + 2, 1);
->>>>>>> origin/tno
 				game_state.enemies.push_back(e);
 				game_state.bullets.push_back(o);
 			}
 			game_state.update();
 			window.render(game_state);
 			window.print_score(game_state, t, time_taken);
-			//usleep(1);
 			game_state.cur_key = wgetch(window.main);
 			if (game_state.cur_key == KEY_RESIZE)
 			{
